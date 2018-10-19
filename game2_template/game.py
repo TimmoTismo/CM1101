cd8d13f4ff5ee06ea8d341dbc8fd5d5a7194daee
TimmoTismo <joshuacapili42@yahoo.co.uk>
2018-10-19 13:37:53 +0100

    Added ASCII 'Congratulations!!'

--- game2_template/game.py
+++ game2_template/game.py
@@ -396,8 +396,16 @@ def main():
         execute_command(command)
 
         if len(rooms["Parking"]["items"]) == 6:
-            print("Congratulations!! You have found the correct room to drop all the items in!!")
-            print()
+            print(""" _____                             _         _       _   _                 _ _ _ 
+/  __ \                           | |       | |     | | (_)               | | | |
+| /  \/ ___  _ __   __ _ _ __ __ _| |_ _   _| | __ _| |_ _  ___  _ __  ___| | | |
+| |    / _ \| '_ \ / _` | '__/ _` | __| | | | |/ _` | __| |/ _ \| '_ \/ __| | | |
+| \__/| (_) | | | | (_| | | | (_| | |_| |_| | | (_| | |_| | (_) | | | \__ |_|_|_|
+ \____/\___/|_| |_|\__, |_|  \__,_|\__|\__,_|_|\__,_|\__|_|\___/|_| |_|___(_(_(_)
+                    __/ |                                                        
+                   |___/                                                         """)
+            print("You have found the correct room to drop all the items in!!")
+            
             break
 
 
